331155ff240aceb6596d90c0bb06452c2bec607d
ricardo <ricardoamaral.sistemas@gmail.com>
2024-03-02 19:37:38 -0300

    layout final

--- app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+        String[] frases = {
+                "Ricardo é o cara!",
+                "Ricardo conseguiu!",
+                "Viva Ricardo!!"
+        };
+        int numero = new Random().nextInt(3);
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText(frases[numero]);
+
     }
 }
